45589272bc6cb880e6d3c77869a5b60257f44d4f
svgnhkmglu <sevginhekimoglu@gmail.com>
2021-04-20 20:04:21 -0400

    TestBase and WindowTabs

---
+++ src/test/java/renastech/base/TestBase.java
@@ -0,0 +1,25 @@
+package renastech.base;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import renastech.utilities.WebDriverUtil;
+
+import java.util.concurrent.TimeUnit;
+
+public class TestBase {
+
+    protected WebDriver driver;
+
+    @BeforeMethod
+    public void setUpMethod(){
+        driver= WebDriverUtil.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    @AfterMethod
+    public void closing(){
+//        driver.close();
+    }
+}
---
+++ src/test/java/renastech/day7/C1_WindowsTabs.java
@@ -0,0 +1,43 @@
+package renastech.day7;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+import renastech.base.TestBase;
+import renastech.utilities.BrowserUtils;
+
+import java.util.Set;
+
+public class C1_WindowsTabs extends TestBase {
+
+    @Test
+    public void TC1_windowTabHandle(){
+        driver.get("http://renastech.com/?amp");
+        String currentWindow=driver.getWindowHandle(); // it is only storing one window
+        //getwindowhandle will store current window that you are working
+        System.out.println("currentWindow = " + currentWindow);
+        System.out.println("driver.getTitle() = " + driver.getTitle());
+
+
+        WebElement openTab=driver.findElement(By.id("opentab")); // we locate open new tab button
+        openTab.click(); // click on open new tab
+        //new tab is getting open
+
+        System.out.println(" After new tab driver.getTitle() = " + driver.getTitle());
+        //since we didnt change focus of driver title will still be same
+        Set<String> allWindows=driver.getWindowHandles();
+
+        for (String window: allWindows){
+            driver.switchTo().window(window);
+            BrowserUtils.wait(2);
+            //you need to switch driver focus on next tab to be able work on that page
+            System.out.println(driver.getTitle());
+        }
+
+
+
+
+    }
+
+
+}
