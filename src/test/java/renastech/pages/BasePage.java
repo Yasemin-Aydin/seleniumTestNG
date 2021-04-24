711beed07c3e06437ed47e856ca25cf7f319000d
svgnhkmglu <sevginhekimoglu@gmail.com>
2021-04-23 21:23:01 -0400

    POM Base PAge

---
+++ src/test/java/renastech/pages/BasePage.java
@@ -0,0 +1,20 @@
+package renastech.pages;
+
+import org.openqa.selenium.support.PageFactory;
+import renastech.utilities.DriverUtil;
+
+public abstract class  BasePage {
+//    IN POM Design you need to have some structure for each page :
+//            1.PageFactory.initElements(driver, this);    this one will be coomen for all page so we are going to store in the base
+
+    //others  is unique for each page
+//		-> It creates connection in between driver and object of the class.
+//            2. @FindBy annotation to locate webElements.
+//            -> Using this FindBy annotation, help us to locate web elements.
+//3. Create methods related webelements
+
+    public BasePage(){
+        PageFactory.initElements(DriverUtil.getDriver(),this);
+    }
+
+}
